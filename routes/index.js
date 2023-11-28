8f184f6d2484dad6b474f720943297aac3ba2737
mariano5troncoso <mariano.nicolas.troncoso@gmail.com>
2023-11-28 19:54:56 -0300

    accesorios NOX

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRouter from './auth.js';
+import accesorios_router from './accessories.js';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/auth', userRouter);
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 export default router;
